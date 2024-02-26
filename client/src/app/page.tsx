9676ab60a5d71aa78eaca686dfd244ec910709af
Grenish rai <ariestheracer@gmail.com>
2024-02-27 04:32:27 +0530

    Added Nextjs client

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { GridBackgroundDemo } from "@/components/ui/GridBackground";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="">
+      <GridBackgroundDemo />
+    </div>
+  );
+};
+
+export default page;
---
+++ client/src/components/ChatInterface.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChatInterface = () => {
+  return (
+    <div className='martine text-5xl text-rose-400  w-full h-screen'>ChatInterface</div>
+  )
+}
+
+export default ChatInterface
