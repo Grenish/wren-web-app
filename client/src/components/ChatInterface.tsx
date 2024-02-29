1f0b9f21479602e421ada84c3147253657f21f16
Grenish rai <ariestheracer@gmail.com>
2024-02-29 07:34:11 +0530

    Refactor ChatInterface component

--- client/src/components/ChatInterface.tsx
+++ client/src/components/ChatInterface.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ChatInterface = () => {
   return (
-    <div className='martine text-5xl text-rose-400  w-full h-screen'>ChatInterface</div>
+    <div className='martine text-5xl text-rose-400  w-full'>
+      
+    </div>
   )
 }
 
